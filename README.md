351e8e946838399106d14e197433586c0a8056c8
Paul Loyd <pavelko95@gmail.com>
2024-04-02 20:45:22 +0200

    chore: README + meta

---
+++ README.md
@@ -0,0 +1,9 @@
+# An efficient concurrent ID to object resolver
+
+An IDR (IDentifier Resolver) provides a way to efficiently and concurrently
+map integer IDs to references to objects. It's particularly useful in
+scenarios where you need to quickly find objects based on their ID.
+
+TODO: example
+
+TODO: graphs
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,4 @@
-//! An efficient concurrent ID to object resolver.
-//!
-//! An IDR (IDentifier Resolver) provides a way to efficiently and concurrently
-//! map integer IDs to references to objects. It's particularly useful in
-//! scenarios where you need to quickly find objects based on their ID.
-//!
-//! The main structure of this crate is [`Idr`].
-//!
-//! TODO
+#![doc = include_str!("../README.md")]
 
 use std::fmt;
 
